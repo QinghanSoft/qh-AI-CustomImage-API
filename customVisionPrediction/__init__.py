d6201dba2192d0d9db338478ece8560451f7821d
dddddkio <r4t5y6u@163.com>
2022-01-13 01:18:15 +0800

    Update Iteration Version

--- customVisionPrediction/__init__.py
+++ customVisionPrediction/__init__.py
@@ -17,7 +17,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     data = { "Url" : imgUrl}
 
-    res = requests.post("https://qhcustomvision-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/c220a5a0-a9a4-4b88-95f9-20cb5890b36d/classify/iterations/Iteration1/url",
+    res = requests.post("https://qhcustomvision-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/c220a5a0-a9a4-4b88-95f9-20cb5890b36d/classify/iterations/Iteration3/url",
         headers = {
             "Prediction-Key" : "1b732bcbe7444a5cadbcec8a99e19fc4",
             "Content-Type" : "application/json"
